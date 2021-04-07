17e1a78d1e8161c573f5183cce63442013b3ccd6
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-04-07 07:53:32 -0300

    chore: add yup

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,15 +1,17 @@
 import { forwardRef, ForwardRefRenderFunction } from 'react'
-import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps, FormErrorMessage } from '@chakra-ui/react'
+import { FieldError } from 'react-hook-form';
 
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError
 }
 
-const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }:InputProps, ref) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, error = null, ...rest }:InputProps, ref) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         name={name}
@@ -25,6 +27,12 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ nam
         ref={ref}
         {...rest}
       />
+
+      { !!error && (
+        <FormErrorMessage>
+          {error.message}
+        </FormErrorMessage>
+      )}
     </FormControl>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Input } from '../components/Form/Input';
 type SignInFormData = {
   email: string;
   password: string;
+  
 }
 
 export default function SignIn() {
@@ -12,6 +13,7 @@ export default function SignIn() {
   const { register, handleSubmit, formState } = useForm();
 
   const { errors } = formState;
+  console.log(errors)
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -36,11 +38,32 @@ export default function SignIn() {
         onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input type="email" name="email" label="Email" {...register("email")} />
-          <Input type="password" name="password" label="Senha" {...register("password")} />
+          <Input 
+            type="email" 
+            name="email" 
+            label="Email" 
+            error={errors.email}
+            {...register('email', { required: 'Email obrigatório '})} 
+          />
+
+          <Input 
+            type="password" 
+            name="password" 
+            label="Senha" 
+            {...register("password")} 
+          />
+
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>Entrar</Button>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink" 
+          size="lg" 
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
 
       </Flex>
     </Flex>
